dfb5a0980c3055d18dc5d98ec03d73f6064d5e87
최진주 <jj.choi@sqisoft.com>
2026-01-08 17:37:03 +0900

    refactor(react): BlogList 컴포넌트 분리 및 Admin/List 재사용 구조로 정리

--- react/react_learn_with_project/react-basic/src/pages/ListPage.js
+++ react/react_learn_with_project/react-basic/src/pages/ListPage.js
@@ -1,79 +1,12 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import Card from '../components/Card';
-import LoadingSpinner from '../components/LoadingSpinner';
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router';
+import BlogList from '../components/BlogList';
 
 const ListPage = () => {
-  const history = useHistory();
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const getPosts = () => {
-    axios.get('http://localhost:3001/posts').then((res) => {
-      setPosts(res.data);
-      setLoading(false);
-    });
-  };
-
-  const deleteBlog = (e, id) => {
-    e.stopPropagation();
-    axios.delete(`http://localhost:3001/posts/${id}`).then(() => {
-      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
-    });
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  const renderBlogList = () => {
-    if (loading) {
-      return <LoadingSpinner />;
-    }
-
-    if (posts.length === 0) {
-      return <div>No blog post found</div>;
-    }
-
-    return posts
-      .filter((post) => {
-        return post.publish;
-      })
-      .map((post) => {
-        return (
-          <Card
-            key={post.id}
-            title={post.title}
-            onClick={() => {
-              history.push(`/blogs/${post.id}`);
-            }}
-          >
-            <div>
-              <button
-                className="btn btn-sm btn-danger"
-                onClick={(e) => deleteBlog(e, post.id)}
-              >
-                Delete
-              </button>
-            </div>
-          </Card>
-        );
-      });
-  };
-
   return (
     <div>
-      <div className="d-flex justify-content-between mb-2">
+      <div className="d-flex mb-2">
         <h1>Blogs</h1>
-        <div>
-          <Link to="/blogs/create" className="btn btn-success">
-            Create New
-          </Link>
-        </div>
       </div>
-      {renderBlogList()}
+      <BlogList />
     </div>
   );
 };
